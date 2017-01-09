ba35a232f73b562e1e7169c24c14fd7487679619
gyamxxx <gyamxxx@gmail.com>
2017-01-10 00:54:12 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -70,6 +70,30 @@ function install(Vue, options = {}) {
             })
           })
         }
+        const axiosHttp = applyOnHttp && Vue.axios
+        if (axiosHttp) {
+          // Add a request interceptor
+          axios.interceptors.request.use(function (config) {
+            // Do something before request is sent
+            initProgress()
+            return config;
+          }, function (error) {
+            // Do something with request error
+            console.error(error)
+            return Promise.reject(error);
+          });
+
+          // Add a response interceptor
+          axios.interceptors.response.use(function (response) {
+            // Do something with response data
+            increase()
+            return response;
+          }, function (error) {
+            // Do something with response error
+            console.error(error)
+            return Promise.reject(error);
+          });
+        }
 
         const router = applyOnRouter && this.$options.router
         if (router) {
